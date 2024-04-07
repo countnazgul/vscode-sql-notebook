92a30791971fb7fafcca010a332b4383a83defec
Stefan Stoichev <stefan.stoichev@gmail.com>
2024-04-07 18:54:44 +0100

    these two was just for test here. will be added later

--- src/serializer.ts
+++ src/serializer.ts
@@ -78,24 +78,3 @@ export class SQLSerializer implements vscode.NotebookSerializer {
     return new TextEncoder().encode(JSON.stringify(cellsData, null, 4));
   }
 }
-
-/**
- * TEMP. Just to test how it works
- */
-export class StatusBarProviderTemp
-  implements vscode.NotebookCellStatusBarItemProvider
-{
-  provideCellStatusBarItems(
-    cell: vscode.NotebookCell
-  ): vscode.NotebookCellStatusBarItem[] | undefined {
-    const openEach = new vscode.NotebookCellStatusBarItem(
-      `Test`,
-      vscode.NotebookCellStatusBarAlignment.Right
-    );
-    openEach.command = 'sqlnotebook.blah';
-    openEach.tooltip = `Temp tooltip`;
-
-    const items: vscode.NotebookCellStatusBarItem[] = [openEach];
-    return items;
-  }
-}
